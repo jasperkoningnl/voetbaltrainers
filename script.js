bc43e05d901a5028b47ba399ca1d6d3f014e35e5
jasperkoningnl <koningjasper@proton.me>
2025-06-18 20:55:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Stap 1: Data laden (deze code hadden we al)
+// Stap 1: Data laden
 d3.csv("engeland.csv").then(function(data) {
     
     console.log("Data succesvol geladen:", data);
@@ -19,16 +19,47 @@ function drawHeatmap(data) {
     const height = 600 - margin.top - margin.bottom;
 
     // --- II. Het SVG-element selecteren en opzetten ---
-    // We selecteren de 'div' met de id 'heatmap-container' die we in de HTML hebben gemaakt.
     const svg = d3.select("#heatmap-container")
-      .append("svg") // We voegen een SVG-element toe aan de div.
+      .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
-      .append("g") // We voegen een 'groep' (<g>) element toe aan de SVG.
-        // Dit wordt onze tekenruimte, verschoven volgens de marges.
+      .append("g")
         .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-    console.log("SVG Canvas en groepselement zijn aangemaakt.");
-    
-    // --- HIER KOMT STRAKS DE REST VAN DE CODE ---
+    // --- III. De domeinen voor de assen bepalen ---
+    // We maken een unieke, gesorteerde lijst van alle seizoenen.
+    const seizoenen = [...new Set(data.map(d => d.Seizoen))].sort();
+    // We maken een unieke lijst van alle clubs.
+    const clubs = [...new Set(data.map(d => d.Club))];
+
+    console.log("Clubs voor y-as:", clubs);
+    console.log("Seizoenen voor x-as:", seizoenen);
+
+    // --- IV. De X- en Y-schalen (assen) definiëren ---
+    // De X-as (seizoenen)
+    const x = d3.scaleBand()
+      .range([ 0, width ]) // De as loopt over de hele breedte
+      .domain(seizoenen) // De 'stapjes' op de as zijn onze seizoenen
+      .padding(0.05); // Een beetje witruimte tussen de blokjes
+
+    // De Y-as (clubs)
+    const y = d3.scaleBand()
+      .range([ height, 0 ]) // De as loopt van onder naar boven
+      .domain(clubs) // De 'stapjes' zijn onze clubs
+      .padding(0.05);
+
+    // --- V. De assen aan de SVG toevoegen ---
+    // Voeg de X-as toe onderaan de visualisatie
+    svg.append("g")
+      .attr("transform", `translate(0, ${height})`) // Positioneer op de bodem
+      .call(d3.axisBottom(x))
+      .selectAll("text") // Selecteer de labels om ze te draaien
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+
+    // Voeg de Y-as toe aan de linkerkant
+    svg.append("g")
+      .call(d3.axisLeft(y));
+
+    // --- HIER KOMT DE CODE VOOR DE GEKLEURDE RECHTHOEKEN ---
 }
