1191a0f7bbe9cb96af591bb52801c51c237ba710
jasperkoningnl <koningjasper@proton.me>
2025-06-29 13:06:34 +0200

    Add Wikipedia coach enrichment function

--- functions/index.js
+++ functions/index.js
@@ -7,9 +7,11 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
+const { setGlobalOptions } = require("firebase-functions");
+const { onRequest } = require("firebase-functions/https");
+const { onCall } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const fetch = require("node-fetch");
 
 // For cost control, you can set the maximum number of containers that can be
 // running at the same time. This helps mitigate the impact of unexpected
@@ -30,3 +32,39 @@ setGlobalOptions({ maxInstances: 10 });
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.enrichCoachData = onCall({ timeoutSeconds: 30 }, async (request) => {
+  const names = Array.isArray(request.data.coaches) ? request.data.coaches : [];
+  const results = [];
+
+  for (const name of names) {
+    let foto_url = "";
+    try {
+      const apiUrl =
+        `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages&titles=${encodeURIComponent(
+          name
+        )}&pithumbsize=400`;
+      const res = await fetch(apiUrl);
+      const data = await res.json();
+      const pages = data.query && data.query.pages;
+      if (pages) {
+        const page = Object.values(pages)[0];
+        const source = page?.thumbnail?.source;
+        if (source) {
+          try {
+            const head = await fetch(source, { method: "HEAD" });
+            if (head.ok) foto_url = source;
+          } catch (err) {
+            logger.error("Failed to verify image", err);
+          }
+        }
+      }
+    } catch (err) {
+      logger.error("Wikipedia lookup failed", err);
+    }
+
+    results.push({ naam: name, nationaliteit: "", nat_code: "", foto_url });
+  }
+
+  return { coaches: results };
+});
